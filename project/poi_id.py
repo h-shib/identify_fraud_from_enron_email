bc3f29501e4ce7b38ab4561181c99192b4dbcac5
h-shib <hiraku.shibuya888@gmail.com>
2016-06-24 00:07:14 +0900

    Add variety of classifiers

--- project/poi_id.py
+++ project/poi_id.py
@@ -8,11 +8,18 @@ from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data, test_classifier
 from sklearn.grid_search import GridSearchCV
 from time import time
+from helper import print_result
 
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi', 'salary', 'exercised_stock_options', 'deferral_payments']
+features_list = ['poi', 'salary', 'exercised_stock_options', 'deferral_payments',
+                 'to_messages', 'total_payments',
+                 'bonus', 'restricted_stock',
+                 'shared_receipt_with_poi', 'restricted_stock_deferred',
+                 'total_stock_value', 'expenses', 'loan_advances', 'from_messages', 
+                 'other', 'from_this_person_to_poi', 'director_fees',
+                 'deferred_income', 'long_term_incentive', 'from_poi_to_this_person']
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
@@ -22,6 +29,7 @@ with open("final_project_dataset.pkl", "r") as data_file:
 ### Task 3: Create new feature(s)
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
+
 # remove outlier
 my_dataset.pop('TOTAL', 0)
 
@@ -38,6 +46,7 @@ features_list.append('fraction_to_poi')
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
 
+
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
 ### Note that if you want to do PCA or other multi-stage operations,
@@ -45,6 +54,29 @@ labels, features = targetFeatureSplit(data)
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
 # Provided to give you a starting point. Try a variety of classifiers.
+
+# Prepare for validation
+from sklearn.cross_validation import train_test_split, cross_val_score
+features_train, features_test, labels_train, labels_test = \
+    train_test_split(features, labels, test_size=0.3, random_state=42)
+
+from sklearn import tree, svm, naive_bayes, neighbors
+
+# DecisionTree
+clf = tree.DecisionTreeClassifier()
+print_result(clf, features_train, labels_train, features_test, labels_test)
+
+# SVM
+clf = svm.SVC()
+print_result(clf, features_train, labels_train, features_test, labels_test)
+
+# Naive Bayes
+clf = naive_bayes.GaussianNB()
+print_result(clf, features_train, labels_train, features_test, labels_test)
+
+# K Nearest Neighbors
+clf = neighbors.KNeighborsClassifier(n_neighbors=3)
+print_result(clf, features_train, labels_train, features_test, labels_test)
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script. Check the tester.py script in the final project
@@ -54,9 +86,6 @@ labels, features = targetFeatureSplit(data)
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
 
 """
 Tuned parameter with GridSearchCV below.
@@ -74,7 +103,6 @@ clf.fit(features_train, labels_train)
 print clf.score(features_test, labels_test)
 """
 
-from sklearn import tree
 clf = tree.DecisionTreeClassifier(criterion='gini', max_depth=None, max_features=None,
                              max_leaf_nodes=None, min_samples_leaf=1, min_samples_split=5)
 
